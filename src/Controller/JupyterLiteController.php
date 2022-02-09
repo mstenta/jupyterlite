bb6678758c688f49d460adc4a869013c359154b0
Michael Stenta <mike@mstenta.net>
2022-02-09 13:38:31 -0500

    Add a /jupyterlite route controller with permission.

---
+++ src/Controller/JupyterLiteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\jupyterlite\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * JupyterLite controller.
+ *
+ * @ingroup jupyterlite
+ */
+class JupyterLiteController extends ControllerBase {
+
+  /**
+   * Builds the JupyterLite index callback.
+   *
+   * @return array
+   *   Render array.
+   */
+  public function index() {
+    return [];
+  }
+
+}
